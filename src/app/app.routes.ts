b71ebce45792711e55167095ff5026d5dcc28d2e
karimul <7mdkarimul@gmail.com>
2024-02-24 23:29:46 +0530

    tailwind added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './admin/dashboard/dashboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {
+        path:"admin",
+        component:DashboardComponent
+    }
+];
